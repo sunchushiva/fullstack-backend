140cbd1d195bf269066378518fd2f25f346df972
Shiva Sunchu <chhotahathi06@gmail.com>
2023-04-29 12:11:21 +0530

    Initial commit

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const todosRoute = require("./routes/todos.route");
 const connection = require("./config/mongo");
 const userRoute = require("./routes/user.route");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/todos", todosRoute);
 app.use("/user", userRoute);
 
